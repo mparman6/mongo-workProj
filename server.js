e6df022b1a050af7991f9341c17b95c1c0fdd6cf
ep85 <ep85@njit.edu>
2016-04-04 16:59:44 -0400

    basic for loop structure and working on concept

--- server.js
+++ server.js
@@ -46,30 +46,52 @@ var materialSchema = Schema({
 
 var Materials = mongoose.model('Materials', materialSchema);
 
-var materialName = new Materials({
-	id: "101",
-    material_category_id: "36",
-    material_name: "Calacatta Borghini",
-    material_original_name: "Calacatta Borghini",
-    material_other_names: "Calcutta Gold, Calacata Gold, Calacatta Doro, Calacatta Vagli, Cremo Delicato, Calacatta Michelangelo ",
-    material_description: "Calacatta Borghini Marble is an exclusive white marble with deep grey veining and occasional taupe highlights. Calacatta Borghini Marble is a natural stone from Italy that adds a little more dynamic range to the customary marble color palette. ",
-    material_online_id: null,
-    material_country_of_origin: "105",
-    main_color: "7",
-    page_title: "Calacatta Borghini Marble a Veiny White Countertop Material.",
-    pattern: "0",
-    last_update: "2015-06-19 09:44:32"
-})
 
+// For Loop to insert materials data into db
+function insertData(dbName, colName, numberOf) {
 
-materialName.save(function(err) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('this material was saved');
+	var materials = [];
+
+	for (var i = 0; i <= materials.length; i++) {
+		
 	}
-});
 
+}
+
+
+
+
+
+
+
+
+
+// var materialName = new Materials({
+// 	id: "101",
+//     material_category_id: "36",
+//     material_name: "Calacatta Borghini",
+//     material_original_name: "Calacatta Borghini",
+//     material_other_names: "Calcutta Gold, Calacata Gold, Calacatta Doro, Calacatta Vagli, Cremo Delicato, Calacatta Michelangelo ",
+//     material_description: "Calacatta Borghini Marble is an exclusive white marble with deep grey veining and occasional taupe highlights. Calacatta Borghini Marble is a natural stone from Italy that adds a little more dynamic range to the customary marble color palette. ",
+//     material_online_id: null,
+//     material_country_of_origin: "105",
+//     main_color: "7",
+//     page_title: "Calacatta Borghini Marble a Veiny White Countertop Material.",
+//     pattern: "0",
+//     last_update: "2015-06-19 09:44:32"
+// })
+
+
+// materialName.save(function(err) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log('this material was saved');
+// 	}
+// });
+
+
+// retrieve doc from db
 Materials.find({
 	material_original_name: "Calacatta Borghini"
 }).
@@ -81,6 +103,8 @@ exec(function(err, material) {
 		console.log(material);
 	}
 });
+
+
 
 
 
